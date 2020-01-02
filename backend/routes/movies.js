c8db05512c077ee42cbdd7feed600d3e1d3d4b37
Marek Sołdaczuk <forexampleee@wp.pl>
2020-01-02 01:31:38 +0100

    more styling, account.html + movie data model

---
+++ backend/routes/movies.js
@@ -0,0 +1,33 @@
+const _ = require('lodash');
+const {Movie,validate} = require('../models/movie.js');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+//create user
+
+
+// do zrobienia
+router.post('/', async (req, res) => {
+    const {error} = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({email: req.body.email});
+    if (user) return res.status(400).send('User already registered.');
+
+    user = new User(_.pick(req.body, ['email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+
+    const token = user.generateAuthToken();
+    res.send(token);    
+});
+
+//get current user
+router.get('/me', auth, async (req,res) => {
+    const user = await User.findById(req.user._id);
+    res.send(_.pick(user, ['_id','email']));
+});
+
+module.exports = router;
