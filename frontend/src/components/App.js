59b05fc379ca15b48ae716756bcc7a409d68db46
Marek Sołdaczuk <forexampleee@wp.pl>
2020-01-05 15:57:57 +0100

    basic react

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MovieList from './MovieList';
+
+// const App = () => {
+//     return <div>App</div>
+// };
+
+class App extends React.Component {
+    render() {
+        return <div><MovieList /></div>
+    }
+}
+
+export default App; 
+
